3de9ecc1c488054e7566803f323cfb859c0e05ae
Ashley Le <ashleyle@usf.edu>
2025-01-26 14:50:06 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,11 @@ var isActive = true; //employee's activity status
 console.log(typeof employeeName);
 console.log(typeof employeeID);
 console.log(typeof isActive);
+
+//Task 2 - Product Details
+let productName = `Lachlan Sad Rat`; //product name
+const productPrice = `33.00`; //product price
+var isAvailable = `true`; //product availability status
+console.log(typeof productName);
+console.log(typeof productPrice);
+console.log(typeof isAvailable);
