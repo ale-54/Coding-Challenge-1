e16600b08fb43d4ef680370ed715b30fcff81555
Ashley Le <ashleyle@usf.edu>
2025-01-26 14:49:28 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+//Task 1 - Employee Information
+let employeeName = `Chicken Nuggs`; //employee's name
+const employeeID = `15632`; //employee's ID
+var isActive = true; //employee's activity status
+console.log(typeof employeeName);
+console.log(typeof employeeID);
+console.log(typeof isActive);
